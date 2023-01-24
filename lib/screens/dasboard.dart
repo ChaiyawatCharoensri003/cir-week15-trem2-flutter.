2dea15746348415eaaf005b9d5a9fc50fe2c9a85
AirSupan <airsupansa23@gmail.com>
2023-01-24 11:31:38 +0700

    updat

---
+++ lib/screens/dasboard.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class Dasboard extends StatelessWidget {
+  const Dasboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text("..."),
+      ),
+      body: Text("..."),
+    );
+  }
+}
